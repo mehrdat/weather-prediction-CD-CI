72c17f83edfccbc903e5df0703c2170b838a53db
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2024-08-28 01:24:57 +0100

    changed  evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,19 +3,46 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import LabelEncoder
 import lightgbm as lgb
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import SimpleImputer,KNNImputer,MissingIndicator,MissingIndicator,IterativeImputer,MissingIndicator
+import numpy as np
+
 
 def load_data(file_path, model_features):
-    data = pd.read_csv(file_path)
+    #data = pd.read_csv(file_path)
     
     # Convert non-numeric columns to numeric
-    for column in data.columns:
-        if data[column].dtype == 'object':
-            data[column] = LabelEncoder().fit_transform(data[column])
+    #for column in data.columns:
+    #    if data[column].dtype == 'object':
+    #        data[column] = LabelEncoder().fit_transform(data[column])
+    
+    
+    df=pd.read_csv(file_path,parse_dates=['date'])
+
+    df['year']=df.date.dt.year
+    df['month']=df.date.dt.month
+
+
+    df=df.apply(lambda x: x.replace(' ',np.nan))
+
+
+    dtypes={'rain':float,'temp':float,'wetb':float,'dewpt':float,
+
+            'vappr':float,'rhum':float,'msl':float,
+            'wdsp':float,'wddir':float}#,'sun':float,'vis':float,'clht':float,'clamt':float}
+    df=df.astype(dtypes)
+    df=df.select_dtypes(include=['number'])
+    df=df.drop(columns=['Unnamed: 0','latitude','longitude'])
+    it=IterativeImputer()
+    df=pd.DataFrame(it.fit_transform(df),columns=df.columns)
+    df=df.drop(['rain','msl','wdsp','wddir','vappr'],axis=1)
+
+
     
     # Ensure the data has the same features as the model was trained on
-    data = data[model_features]
+    #data = data[model_features]
     
-    return data
+    return df
 
 def evaluate_model(model_path, data_path):
     model = joblib.load(model_path)
